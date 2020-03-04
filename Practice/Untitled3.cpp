08b63c9e8591b8d5d2631b50f42afe229327e3bd
Basharat Hussain <31223223+Basharat151214@users.noreply.github.com>
2020-03-04 19:09:54 +0500

    a

---
+++ Practice/Untitled3.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	void add();
+	
+	void add() {
+		int b = 5;
+		int d = 9;
+		int a;
+		a = b+d;
+		cout<<a;
+		
+	}	
+	
+	add();
+	
+	return 0;
+}
+
+
+
